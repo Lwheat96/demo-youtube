fba4bdfceced8c945a3933e9f5ff2c70170b5bcd
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-14 09:14:00 -0400

    It was working for a second but not now

--- test-cpp.cpp/main.cpp
+++ test-cpp.cpp/main.cpp
@@ -12,6 +12,19 @@ int main() {
 
     for (int i = 1; i <= n; ++i) {
         // prints first two terms
-        if (i )
+        if (i == 1) {
+            std::cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            std::cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        std::cout << nextTerm << ", ";
     }
+    return 0;
 }
