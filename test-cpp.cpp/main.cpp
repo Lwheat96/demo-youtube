cbbd8f64c80e2f484c5184c47631bf551d368adb
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-14 09:00:01 -0400

    Initializing fixed the 10000?

---
+++ test-cpp.cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+/* 
+* @brief sample fibonacci program
+* 
+* @return int
+*/
+
+int main() {
+    int n = 15, t1 = 0, t2 = 1, nextTerm = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        // prints first two terms
+        if (i )
+    }
+}
